8c75b4eb7b457d26c3de7ae833cbdf07def9f9d5
ValeriaNiceria <valerianiceria@gmail.com>
2018-02-19 21:09:56 -0300

    Conexão com banco de dados

---
+++ conexao.php
@@ -0,0 +1,18 @@
+<?php
+
+include("config.php");
+
+try {
+
+	//Conecta com MySQL usando PDO
+	$conexao = new PDO("mysql:dbname=" . BD_BANCO . ";host=" . BD_SERVIDOR, BD_USUARIO, BD_SENHA);
+
+	//O método setAttribute() permite adicionar atributos no objeto de conexão
+	$conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	//Informa ao servidor de banco de dados, que será utilizada palavras com acento
+	$conexao->exec("set name utf-8");
+	
+}catch(PDOException $erro) {
+	echo "Erro na conexão: " . $erro->getMessage();
+}
---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+//Constantes de conexão ao banco de dados
+
+define("BD_SERVIDOR", "127.0.0.1");
+define("BD_USUARIO", "root");
+define("BD_SENHA", "54321");
+define("BD_BANCO", "crud_simples");
