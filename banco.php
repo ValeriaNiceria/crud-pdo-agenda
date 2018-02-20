ac444de714729acbe2c872d934a044d5c3b9c64a
ValeriaNiceria <valerianiceria@gmail.com>
2018-02-20 14:12:18 -0300

    DELETE - Excluíndo registro do banco de dados

--- banco.php
+++ banco.php
@@ -2,14 +2,15 @@
 
 require_once("conexao.php");
 
-//CREATE - Salvando novo registro no banco de dados
 
 if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "save" && $nome != "") {
     try {
         if ($id != "") {
+        	//UPDATE - Alterando registros salvos no banco
             $stmt = $conexao->prepare("UPDATE contatos SET nome=?, email=?, telefone=? WHERE id = ?");
             $stmt->bindParam(4, $id);
         } else {
+        	//CREATE - Salvando novo registro no banco de dados
             $stmt = $conexao->prepare("INSERT INTO contatos (nome, email, telefone) VALUES (?, ?, ?)");
         }
         $stmt->bindParam(1, $nome);
@@ -50,6 +51,25 @@ if (isset($_REQUEST['act']) && $_REQUEST['act'] == 'upd' && $id != "") {
 		} else {
 			throw new PDOException("Erro: Não foi possível executar a declaração sql");
 		}
+	} catch (PDOException $erro) {
+		$mensagem_erro = "Erro: " . $erro->getMessage();
+	}
+}
+
+//DELETE - Excluíndo registro do banco de dados
+
+if (isset($_REQUEST['act']) && $_REQUEST['act'] == 'del' && $id != "") {
+	try {
+		$stmt = $conexao->prepare("DELETE FROM contatos WHERE id = ?");
+		$stmt->bindParam(1, $id, PDO::PARAM_INT);
+
+		if ($stmt->execute()) {
+			$mensagem_success = "Registro foi excluído com sucesso!";
+			$id = NULL;
+		} else {
+			throw new PDOException("Erro: Não foi possível executar a declaração sql");
+		}
+
 	} catch (PDOException $erro) {
 		$mensagem_erro = "Erro: " . $erro->getMessage();
 	}
